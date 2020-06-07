d3dc16490819ddbe9d1d48629d806befa09bc49f
chooomedia <christopher@chooomedia.de>
2020-06-07 22:59:12 +0200

    added dynamic admin-page-modules

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -3,24 +3,63 @@
 namespace inc\Pages;
 
 use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
 
-/**
- * 
- * 
- */
 
- class Admin extends BaseController
- {
+class Admin extends BaseController
+{
+    public $settings;
 
-   public function register() {
-      add_action( 'admin_menu', array( $this, 'add_admin_pages') );
-   }
+    public $pages = array();
 
-   public function add_admin_pages() {
-      add_menu_page( 'Fooorm Plugin', 'Fooorm', 'manage_options', 'fooorm_plugin', array( $this, 'admin_index' ), 'dashicons-format-status', 110 );
-   }
+    public $subpages = array();
 
-   public function admin_index() {
-         require_once $this->plugin_path . 'templates/admin.php';
-   }
+    public function __construct()
+    {
+        $this->settings = new SettingsApi();
+
+        $this->pages = array(
+            array(
+                'page_title' => 'Fooorm Plugin',
+                'menu_title' => 'Fooorm',
+                'capability' => 'manage_options',
+                'menu_slug' => 'fooorm_plugin',
+                'callback' => function() { echo '<h1>Fooorm Plugin</h1>'; },
+                'icon_url' => 'dashicons-format-status',
+                'position' => 110
+            )
+        );
+
+        $this->subpages = array(
+            array(
+                'parent_slug' => 'fooorm_plugin',
+                'page_title' => 'Custom Post Types',
+                'menu_title' => 'CPT',
+                'capability' => 'manage_options',
+                'menu_slug' => 'fooorm_cpt',
+                'callback' => function() { echo '<h1>CPT Manager</h1>'; }
+            ),
+            array(
+                'parent_slug' => 'fooorm_plugin',
+                'page_title' => 'Custom Taxonomies',
+                'menu_title' => 'Taxonomies',
+                'capability' => 'manage_options',
+                'menu_slug' => 'fooorm_taxonomies',
+                'callback' => function() { echo '<h1>Taxonomies Manager</h1>'; }
+            ),
+            array(
+                'parent_slug' => 'fooorm_plugin',
+                'page_title' => 'Custom Widgets',
+                'menu_title' => 'Widgets',
+                'capability' => 'manage_options',
+                'menu_slug' => 'fooorm_widgets',
+                'callback' => function() { echo '<h1>Widgets Manager</h1>'; }
+            )
+        );
+    }
+
+    public function register()
+    {
+        $this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
+    }
 }
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Fooorm Plugin v1.0 Series
+
+Full list of sections we will build during the series of Tutorials
+
+* Modular Administration Area
+* CPT Manager
+* Custom Taxonomy Manager
+* Widget to Upload and Display media in sidebars
+* Post and Pages Gallery integration
+* Testimonial section: Comment in the fronte-end, Admins can approve commetns, select which comments to display
+* Custom template sections
+* Ajax based Login/Register system
+* Membership protected area
+* Chat system
